76a5579274fd0dfa89afbc4d57c1e4d6a06e06bb
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2019-02-10 09:17:40 +0100

    Add libfuzzer

---
+++ fuzz/fuzz_targets/all.rs
@@ -0,0 +1,22 @@
+#![allow(unused_must_use)]
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate loe;
+
+use std::io::Cursor;
+
+use loe::{process, Config, Encoding, TransformMode};
+
+fuzz_target!(|data: &[u8]| {
+    let mut input = Cursor::new(data);
+    let mut output = Cursor::new(Vec::<u8>::new());
+
+    for enc in &[Encoding::Ignore, Encoding::Ascii, Encoding::Utf8] {
+        for trans in &[TransformMode::LF, TransformMode::CRLF] {
+            let config = Config::default().encoding(*enc).transform(*trans);
+            // do not call `unwrap` because we do not check for errors (io error, encoding error, etc.)
+            process(&mut input, &mut output, config);
+        }
+    }
+});
---
+++ fuzz/fuzz_targets/utf8.rs
@@ -0,0 +1,21 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate loe;
+
+use std::io::Cursor;
+
+use loe::{process, Config, Encoding, TransformMode};
+
+fuzz_target!(|data: &[u8]| {
+    // only utf8 strings
+    if let Ok(s) = std::str::from_utf8(data) {
+        let mut input = Cursor::new(s);
+        let mut output = Cursor::new(Vec::<u8>::new());
+
+        for trans in &[TransformMode::LF, TransformMode::CRLF] {
+            let config = Config::default().encoding(Encoding::Utf8).transform(*trans);
+            process(&mut input, &mut output, config).unwrap();
+        }
+    }
+});
